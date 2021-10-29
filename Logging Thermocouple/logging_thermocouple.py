c48c8fb98ca94e6f4a5dcbfc68070c54e29601cc
Matt Swain <m.swain@vernalis.com>
2021-10-29 12:04:51 +0100

    Use snake case not camel case for variables

--- Logging Thermocouple/logging_thermocouple.py
+++ Logging Thermocouple/logging_thermocouple.py
@@ -69,17 +69,17 @@ while True:
 
     # Get temp, monitor error in reading sensor (~1.3% failure rate)
     try:
-        tempC = max31855.temperature
+        temp_c = max31855.temperature
         # Append temp to file with timestamp
         with open(file,'a',newline='') as f:
             writer = csv.writer(f, delimiter=',')
             date_str = str(date.today())
             time_str = datetime.now().strftime('%H:%M:%S')
-            writer.writerow([date_str, time_str, tempC])
+            writer.writerow([date_str, time_str, temp_c])
         try_count = 0
         lcd.clear()
-        lcd.message = "Temp: {}".format(tempC)
-        print(tempC)
+        lcd.message = "Temp: {}".format(temp_c)
+        print(temp_c)
     except RuntimeError:
         try_count = try_count+1
         print(try_count)
