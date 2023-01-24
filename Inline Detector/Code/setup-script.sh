7ed6557a1e6f8500adc62ea3a0a8fc70a90752a8
James Davidson <j.davidson@vernalis.com>
2023-01-24 18:48:58 +0000

    Update setup script

--- Inline Detector/Code/setup-script.sh
+++ Inline Detector/Code/setup-script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update \
- && sudo apt-get install python3-pip -y \
- && sudo pip3 install --upgrade --no-input adafruit-python-shell numpy pandas matplotlib \
+ && sudo apt-get install python3-pip libatlas3-base -y \
+ && sudo pip3 install --upgrade --no-input \
+  adafruit-python-shell \
+  adafruit-circuitpython-as7341 \
+  matplotlib \
+  numpy \
+  pandas \
  && wget https://raw.githubusercontent.com/vernalis/digital-lab/main/Inline%20Detector/Code/inline_detector.py \
  && wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py \
  && sudo python3 raspi-blinka.py \
